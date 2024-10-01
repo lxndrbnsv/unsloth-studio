f8cf0d012d5397354aac1134f0274a95680e5e91
lxndrbnsv <abnosov@gmail.com>
2024-10-01 09:47:25 +0200

    reformatted

--- unsloth_studio/chat.py
+++ unsloth_studio/chat.py
@@ -17,6 +17,7 @@
 from IPython.display import clear_output
 import subprocess
 import os
+
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
 os.environ["GRADIO_ANALYTICS_ENABLED"] = "False"
 MODEL_NAME = "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit"
@@ -24,14 +25,18 @@ MODEL_NAME = "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit"
 print("Installing packages... Please wait 1 minute ...")
 
 install_first = [
-    "pip", "install",
-    "huggingface_hub", "hf_transfer", "triton",
+    "pip",
+    "install",
+    "huggingface_hub",
+    "hf_transfer",
+    "triton",
 ]
 install_first = subprocess.Popen(install_first)
 install_first.wait()
 
 install_second = [
-    "pip", "install",
+    "pip",
+    "install",
     "gradio",
     "unsloth[colab-new]@git+https://github.com/lxndrbnsv/unsloth",
 ]
@@ -39,24 +44,46 @@ install_second = subprocess.Popen(install_second)
 
 from huggingface_hub import snapshot_download
 import warnings
-warnings.filterwarnings(action = "ignore", category = UserWarning,    module = "torch")
-warnings.filterwarnings(action = "ignore", category = UserWarning,    module = "huggingface_hub")
-warnings.filterwarnings(action = "ignore", category = FutureWarning,  module = "huggingface_hub")
-warnings.filterwarnings(action = "ignore", category = RuntimeWarning, module = "subprocess")
-warnings.filterwarnings(action = "ignore", category = UserWarning,    module = "transformers")
-warnings.filterwarnings(action = "ignore", category = FutureWarning,  module = "accelerate")
-warnings.filterwarnings(action = "ignore", category = RuntimeWarning, module = "multiprocessing")
-warnings.filterwarnings(action = "ignore", category = RuntimeWarning, module = "multiprocess")
+
+warnings.filterwarnings(action="ignore", category=UserWarning, module="torch")
+warnings.filterwarnings(
+    action="ignore", category=UserWarning, module="huggingface_hub"
+)
+warnings.filterwarnings(
+    action="ignore", category=FutureWarning, module="huggingface_hub"
+)
+warnings.filterwarnings(
+    action="ignore", category=RuntimeWarning, module="subprocess"
+)
+warnings.filterwarnings(
+    action="ignore", category=UserWarning, module="transformers"
+)
+warnings.filterwarnings(
+    action="ignore", category=FutureWarning, module="accelerate"
+)
+warnings.filterwarnings(
+    action="ignore", category=RuntimeWarning, module="multiprocessing"
+)
+warnings.filterwarnings(
+    action="ignore", category=RuntimeWarning, module="multiprocess"
+)
 
 from huggingface_hub.utils import disable_progress_bars
+
 disable_progress_bars()
-snapshot_download(repo_id = MODEL_NAME, repo_type = "model")
+snapshot_download(repo_id=MODEL_NAME, repo_type="model")
 
 install_second.wait()
 
 install_dependencies = [
-    "pip", "install", "--no-deps",
-    "xformers", "trl", "peft", "accelerate", "bitsandbytes",
+    "pip",
+    "install",
+    "--no-deps",
+    "xformers",
+    "trl",
+    "peft",
+    "accelerate",
+    "bitsandbytes",
 ]
 install_dependencies = subprocess.Popen(install_dependencies)
 install_dependencies.wait()
@@ -66,48 +93,67 @@ clear_output()
 from contextlib import redirect_stdout
 import io
 import logging
-logging.getLogger("transformers.utils.hub").setLevel(logging.CRITICAL+1)
+
+logging.getLogger("transformers.utils.hub").setLevel(logging.CRITICAL + 1)
 
 print("Loading model ... Please wait 1 more minute! ...")
 
 with redirect_stdout(io.StringIO()):
     from unsloth import FastLanguageModel
     import torch
+
     model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = MODEL_NAME,
-        max_seq_length = None,
-        dtype = None,
-        load_in_4bit = True,
+        model_name=MODEL_NAME,
+        max_seq_length=None,
+        dtype=None,
+        load_in_4bit=True,
     )
     FastLanguageModel.for_inference(model)
 pass
 clear_output()
 
 import gradio
+
 gradio.strings.en["SHARE_LINK_DISPLAY"] = ""
-from transformers import TextIteratorStreamer, StoppingCriteria, StoppingCriteriaList
+from transformers import (
+    TextIteratorStreamer,
+    StoppingCriteria,
+    StoppingCriteriaList,
+)
 from threading import Thread
+
 
 class StopOnTokens(StoppingCriteria):
     def __init__(self, stop_token_ids):
         self.stop_token_ids = tuple(set(stop_token_ids))
+
     pass
 
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+    def __call__(
+        self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs
+    ) -> bool:
         return input_ids[0][-1].item() in self.stop_token_ids
+
     pass
+
+
 pass
+
 
 def async_process_chatbot(message, history):
     eos_token = tokenizer.eos_token
-    stop_on_tokens = StopOnTokens([eos_token,])
-    text_streamer  = TextIteratorStreamer(tokenizer, skip_prompt = True)
+    stop_on_tokens = StopOnTokens(
+        [
+            eos_token,
+        ]
+    )
+    text_streamer = TextIteratorStreamer(tokenizer, skip_prompt=True)
 
     # From https://www.gradio.app/guides/creating-a-chatbot-fast
     history_transformer_format = history + [[message, ""]]
     messages = []
     for item in history_transformer_format:
-        messages.append({"role": "user",      "content": item[0]})
+        messages.append({"role": "user", "content": item[0]})
         messages.append({"role": "assistant", "content": item[1]})
     pass
     # Remove last assistant and instead use add_generation_prompt
@@ -115,54 +161,60 @@ def async_process_chatbot(message, history):
 
     input_ids = tokenizer.apply_chat_template(
         messages,
-        add_generation_prompt = True,
-        return_tensors = "pt",
-    ).to("cuda", non_blocking = True)
+        add_generation_prompt=True,
+        return_tensors="pt",
+    ).to("cuda", non_blocking=True)
 
     # Add stopping criteria - will not output EOS / EOT
     generation_kwargs = dict(
-        input_ids = input_ids,
-        streamer = text_streamer,
-        max_new_tokens = 1024,
-        stopping_criteria = StoppingCriteriaList([stop_on_tokens,]),
-        temperature = 0.7,
-        do_sample = True,
+        input_ids=input_ids,
+        streamer=text_streamer,
+        max_new_tokens=1024,
+        stopping_criteria=StoppingCriteriaList(
+            [
+                stop_on_tokens,
+            ]
+        ),
+        temperature=0.7,
+        do_sample=True,
     )
-    thread = Thread(target = model.generate, kwargs = generation_kwargs)
+    thread = Thread(target=model.generate, kwargs=generation_kwargs)
     thread.start()
 
     # Yield will save the output to history!
     generated_text = ""
     for new_text in text_streamer:
         if new_text.endswith(eos_token):
-            new_text = new_text[:len(new_text) - len(eos_token)]
+            new_text = new_text[: len(new_text) - len(eos_token)]
         generated_text += new_text
         yield generated_text
     pass
+
+
 pass
 
 studio_theme = gradio.themes.Soft(
-    primary_hue = "teal",
+    primary_hue="teal",
 )
 
 scene = gradio.ChatInterface(
     async_process_chatbot,
-    chatbot = gradio.Chatbot(
-        height = 325,
-        label = "Чат",
+    chatbot=gradio.Chatbot(
+        height=325,
+        label="Чат",
     ),
-    textbox = gradio.Textbox(
-        placeholder = "Введите сообщение",
-        container = False,
+    textbox=gradio.Textbox(
+        placeholder="Введите сообщение",
+        container=False,
     ),
-    title = None,
-    theme = studio_theme,
-    examples = None,
-    cache_examples = False,
-    retry_btn = None,
-    undo_btn = "Удалить предыдущее сообщение",
-    clear_btn = "Перезапустить чат",
-    css="footer {visibility: hidden}"
+    title=None,
+    theme=studio_theme,
+    examples=None,
+    cache_examples=False,
+    retry_btn=None,
+    undo_btn="Удалить предыдущее сообщение",
+    clear_btn="Перезапустить чат",
+    css="footer {visibility: hidden}",
 )
 
-scene.launch(quiet = True)
+scene.launch(quiet=True)
